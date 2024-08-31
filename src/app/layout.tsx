cf5534376e1a980e0468e02f914e1bcb1bb020e8
50516004 <iwakura.kenshin.15@shizuoka.ac.jp>
2024-08-31 19:12:35 +0900

    manifest変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,6 @@ export default function RootLayout({
     <html lang="ja">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png"></link>
-        <meta name="theme-color" content="#b8e986" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
